b449f01c31308f747340dfc18c61b38d4f8052e1
domfrbassett <dominic.bassett@mail.bcu.ac.uk>
2025-01-28 16:33:07 +0000

    Updated with var

--- Test.m
+++ Test.m
@@ -1,3 +1,7 @@
-%%test
-bucd iucubdg yucdbicyddbuckhcsbyiscb
-bduchbucicndscydnciduhcdsncgdocnygscodc dgsdncodsinc uydgcndcodusihcdocdncoischdc
+Fs = 44100; % Sample rate in Hz
+dur = 3; % Duration in seconds
+freq = 100; % Frequency in Hz
+theta = 0; % Phase offset in radians
+A = 0.5;
+sig = DomsSineWave(A, freq,Fs, dur, theta);
+sound(sig, Fs);
