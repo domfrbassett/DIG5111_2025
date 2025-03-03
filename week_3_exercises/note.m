69ae662959bcf0f9ac44b3a8fc9916fdb9988dc2
domfrbassett <dominic.bassett@mail.bcu.ac.uk>
2025-03-03 22:27:46 +0000

    edited task

--- week_3_exercises/note.m
+++ week_3_exercises/note.m
@@ -1,4 +1,4 @@
-function noteX = note(A, keynum, dur)
+function [noteX, t] = note(A, keynum, dur)
 %function note() will create a note with sampling frequency 11025 by given
 %any key and any time duration.
 Fs = 11025;
@@ -7,7 +7,7 @@ A4=440;
 ref_key=49;     %A4 is the 49th Key in piano keyboard 
 n = keynum-ref_key;  %Calculate the difference between ref_key and input Key
 freq = A4*2^(n/12); %Calculate the frequency of input Key
-Time = 0:Ts:dur;
+t = 0:Ts:dur;
 
-noteX=A*sin(2*pi*freq*Time);
+noteX=A*sin(2*pi*freq*t);
 end
